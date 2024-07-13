984c30ce3a2035167de6dc8e030cf89dc9f060bc
Ananya <96562914+ananyacsa@users.noreply.github.com>
2024-07-13 16:56:20 -0400

    Comments and bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-"# GroceryListApp" 
+# GroceryListApp
+
+GroceryListApp is a simple Android application for creating and managing a grocery list. User needs to enter the quantity and Item name which will be then added to the list
+
+## Features
+
+- Add items with a specified quantity to the grocery list
+- Delete items from the grocery list
+- Enabling/disabling of the add button based on whether user has added any data or not
+- Custom adapter for displaying grocery items (as normal list view didn't look very appealing visually)
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- Minimum Android Version: Android 7.0 (Nougat) (API level 24)
+- Target Android Version: Android 14 (API level 34)
+- Compile SDK Version: 34
+- Java Version: 1.8
+- Android Studio Koala | 2024.1.1
+- An Android device or emulator running Android 7.0 (Nougat) or higher
+
+### Installing
+
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/yourusername/GroceryListApp.git
+    ```
+
+2. **Open the project in Android Studio:**
+
+    - Open Android Studio
+    - Click on `File -> Open`
+    - Navigate to the cloned repository and open it
+
+3. **Build the project:**
+    - Click on `Build -> Clean Project` to build the project
+    - Click on `Build -> Rebuild Project` to build the project
+
+4. **Run the app:**
+
+    - Click on `Run -> Run 'app'` to run the application on an emulator or connected device (Make sure to configure a device using the device manager either an emulator or mobile phone)
+
+## Usage
+
+1. **Add an item:**
+
+    - Enter the item name in the text field
+    - Select the quantity from the dropdown spinner
+    - Click the `Add Item` button to add the item to the list
+
+2. **Delete an item:**
+
+    - Click the `Delete` button next to the item in the list to remove it
+
+## Code Overview
+
+- **MainActivity:** The main activity of the application that handles the UI components and user interactions.
+- **GroceryAdapter:** A custom adapter to manage the display of grocery items in the `ListView`.
+
+### MainActivity
+
+The `MainActivity` initializes the UI components, sets up the adapters, and handles user interactions such as adding and deleting items from the list.
+
+### GroceryAdapter
+
+The `GroceryAdapter` extends `ArrayAdapter<Pair<String, String>>` and provides a custom view for each item in the grocery list. It handles the display of the item name, quantity, and delete button.
+
+## Acknowledgments
+- https://developer.android.com/reference/kotlin/android/widget/Adapter
+- https://www.geeksforgeeks.org/kotlin-android-tutorial/
+- https://www.javatpoint.com/kotlin-tutorial
+- https://www.udemy.com/course/kotlin-masterclass-learn-kotlin-from-zero-to-advanced/learn/lecture/32602270?start=75#overview
--- app/src/main/java/com/example/grocerylistapp/MainActivity.kt
+++ app/src/main/java/com/example/grocerylistapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.grocerylistapp
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // We are initializing all the needed vars to be used in onCreate to prevent adding nullchecks later
     private lateinit var itemEditText: EditText
     private lateinit var quantitySpinner: Spinner
     private lateinit var addItemButton: Button
@@ -19,6 +22,13 @@ class MainActivity : AppCompatActivity() {
     private val groceryItems = mutableListOf<Pair<String, String>>()
     private lateinit var adapter: GroceryAdapter
 
+    private var isItemValid = false
+    private var isQuantityValid = false
+
+    /*
+     * The onCreate method is called on creation of.
+     * Here we do the starting setup
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,7 +39,10 @@ class MainActivity : AppCompatActivity() {
         groceryListView = findViewById(R.id.groceryListView)
         totalItemsTextView = findViewById(R.id.totalItemsTextView)
 
+        // Values to be displayed in spinner
         val quantities = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+
+        // quantityAdapter is used to provide data to the Spinner.
         val quantityAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, quantities)
         quantityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         quantitySpinner.adapter = quantityAdapter
@@ -40,32 +53,60 @@ class MainActivity : AppCompatActivity() {
         quantitySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 val selectedQuantity = parent.getItemAtPosition(position).toString().toInt()
-                addItemButton.isEnabled = selectedQuantity != 0
+                isQuantityValid = selectedQuantity != 0
+                updateAddButtonState()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
+        // TextWatcher to monitor text changes in itemEditText
+        itemEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                isItemValid = !s.isNullOrEmpty()
+                updateAddButtonState()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // Listener for add button
         addItemButton.setOnClickListener {
             val item = itemEditText.text.toString()
             val quantity = quantitySpinner.selectedItem.toString()
 
+            // Once we have added the item, the spinner and edit text is
+            // reset and button is disabled
             if (item.isNotEmpty()) {
                 groceryItems.add(Pair(item, quantity))
                 adapter.notifyDataSetChanged()
                 itemEditText.text.clear()
                 quantitySpinner.setSelection(0)
+                isItemValid = false
+                isQuantityValid = false
+                updateAddButtonState()
                 updateTotalItems()
             }
         }
-
-        updateTotalItems()
     }
 
+    // Update the state of the add button based on item and quantity validity
+    private fun updateAddButtonState() {
+        addItemButton.isEnabled = isItemValid && isQuantityValid
+    }
+
+    // We have a variable in the app which counts the number of items added. This updates it.
     private fun updateTotalItems() {
         totalItemsTextView.text = getString(R.string.total_items, groceryItems.size)
     }
 
+    /**
+     * Custom adapter to manage the display of grocery items in the ListView.
+     * We are trying to display and icon, item name and item quantity.
+     * This adapter helps in controlling all of these better.
+     */
     inner class GroceryAdapter(context: Context, private val items: MutableList<Pair<String, String>>) :
         ArrayAdapter<Pair<String, String>>(context, 0, items) {
 
@@ -90,3 +131,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
